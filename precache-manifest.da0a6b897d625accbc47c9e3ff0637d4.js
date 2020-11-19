e091a5c91751a745a4fc34ba1fcc5d12cee7e6db
AntonChetveryk <chetverykanton92@gmail.com>
2020-11-19 14:50:02 +0200

    Updates

---
+++ precache-manifest.da0a6b897d625accbc47c9e3ff0637d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce85244af5afabb051ee095e9a22f52c",
+    "url": "/react_people-table-advanced/index.html"
+  },
+  {
+    "revision": "15ac99583ab7a1b3eaef",
+    "url": "/react_people-table-advanced/static/css/main.a0e0eacb.chunk.css"
+  },
+  {
+    "revision": "f4ef5e5ac854bb9f8b72",
+    "url": "/react_people-table-advanced/static/js/2.85397912.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/react_people-table-advanced/static/js/2.85397912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ac99583ab7a1b3eaef",
+    "url": "/react_people-table-advanced/static/js/main.d70df36f.chunk.js"
+  },
+  {
+    "revision": "c9836782c33f907f6080",
+    "url": "/react_people-table-advanced/static/js/runtime-main.072d365a.js"
+  }
+]);
